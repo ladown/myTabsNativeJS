37e91c654e2116221496efb32bb8c93128efcf19
Egor Ermilov <egorka33rus52@gmail.com>
2020-11-06 21:04:09 +0300

    first_upload

---
+++ tabs.js
@@ -0,0 +1,45 @@
+"use strict";
+
+function tabs(tabsSelector, tabsContentSelector, tabsParantSelector, activeClass) {
+
+    const tabs = document.querySelectorAll(tabsSelector),
+          tabsContent = document.querySelectorAll(tabsContentSelector),
+          tabsParant = document.querySelector(tabsParantSelector);
+
+    function hideTabContent () {
+    tabsContent.forEach( item => {
+        item.classList.add(`hide`);
+        item.classList.remove(`show`, `fade`);
+    });
+
+    tabs.forEach(item => {
+        item.classList.remove(activeClass);
+    });
+    }
+
+    function showTabContent (i = 0) {
+    tabsContent[i].classList.add(`show`, `fade`);
+    tabsContent[i].classList.remove(`hide`);
+    tabs[i].classList.add(activeClass);
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParant.addEventListener(`click`, (event) => {
+    
+    const target = event.target;
+
+    if (target && target.classList.contains(tabsSelector.slice(1)) ) {
+        tabs.forEach((item, i) => {
+            if (target == item) {
+            hideTabContent();
+            showTabContent(i);
+            } 
+        });
+    }
+    });
+}
+
+
+export default tabs;
